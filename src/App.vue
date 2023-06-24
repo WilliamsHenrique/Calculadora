00ea0a5cbf02b556ec3adddb7bc52f6d6ebec800
Williams Henrique <87279572+WilliamsHenrique@users.noreply.github.com>
2023-06-24 16:51:59 -0300

    calculadora vue

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import Calculator from './components/Calculadora.vue'
+</script>
+
+<template>
+  <Calculator />
+</template>
+
+<style>
+  body{
+    background: #31475e;
+    background-image: url(./image/image.jpg);
+  }
+</style>
---
+++ src/components/Calculadora.vue
@@ -0,0 +1,98 @@
+
+<script>
+import { getCurrentInstance } from 'vue';
+
+export default {
+  name: 'Calculator',
+  props: {
+    msg: String
+  },
+
+  data() {
+    return {
+      calcValor: '',
+      calcNum: ['C', '%', '=', '+', 7, 8, 9,'-', 4, 5, 6,'*', 1, 2, 3, '/', 0, '.'],
+      operadores: null,
+      prevcalcValor: ''
+    }
+  }, 
+  methods: {
+    action(btn){
+
+      if(!isNaN(btn) || btn === '.')
+      {
+        this.calcValor += btn +''
+      }
+
+      if (btn === 'C') {
+        this.calcValor = ''
+      }
+
+      if (btn === '%') {
+        this.calcValor = this.calcValor / 100 + ''
+      }
+
+      if (['/', '+', '-', '*'].includes(btn)) {
+        this.operadores = btn
+        this.prevcalcValor = this.calcValor
+        this.calcValor = ''
+      }
+
+      if (btn === "=") {
+          this.calcValor = eval(
+            this.prevcalcValor + this.operadores + this.calcValor
+            )
+          this.prevcalcValor = ''
+          this.operadores = null
+      }
+    }
+  },
+}
+</script>
+
+
+
+<template>
+  <div class="p-3" style="max-width: 400px; margin: 50px auto; background-color: #234;">
+
+    <!-- Calculator Result -->
+    <div class="w-full rounded m-1 p-3 text-end lead fw-bold text-white digitos">
+      {{ calcValor || 0}}
+    </div>  
+
+    <!-- Calculator Buttons -->
+    <div class="row g-0">
+      <div class="col-3" v-for="btn in calcNum">
+        <div 
+        class="lead text-white text-center m-1 py-3 digitos rounded resultado"
+        :class="{'operadores': ['C', '*', '/', '+', '-', '=', '%'].includes(btn)}"
+        @click="action(btn)"
+          >
+          {{ btn }}
+        </div>
+      </div>
+    </div>
+  </div>    
+</template>
+
+
+<style scoped>
+.digitos{
+  background: rgba(16, 56, 88, 0.5);
+  backdrop-filter: blur(10px);
+  max-width: 350px;
+  margin: 50px auto 0;
+  border-radius: 20px;
+  box-shadow: 0 10px 30px 1px rgba(0,0,0,.5);
+  overflow: hidden; 
+}
+
+.resultado:hover{
+  cursor: pointer;
+  background: #4684c7;
+}
+
+.operadores{
+  background: #3fb984 ;
+}
+</style>
